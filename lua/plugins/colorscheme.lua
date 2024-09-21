51152fdd5fe674843110f24fbf24b060fb340497
Aaron <aaronvendedor@gmail.com>
2024-09-21 09:56:27 -0400

    Fixed client rest and changed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,7 +10,7 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
-      vim.o.background = "light"
+      vim.o.background = "dark"
       vim.g.everforest_enable_italic = true
       vim.g.everforest_trasparent_background = 2
       vim.cmd.colorscheme("everforest")
